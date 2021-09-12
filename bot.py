c57e215e9ad372f3b1553c1af32fb752277e9266
ycwhencpp <anurag.jay1002@gmail.com>
2021-09-12 10:57:15 -0700

    add like and retweet task

--- bot.py
+++ bot.py
@@ -1,7 +1,12 @@
 from selenium import webdriver    # to control browser operations
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 import time
-from config import username,password
+from config import username,password,hashtag #to pass login credentials
+
+
+#asking user about how many page to load 
+load_pages=int(input("How many pages you want to load:"))
 
 
 #creating bot class
@@ -10,10 +15,13 @@ class TwitterBot:
     def __init__(self,username,password):
         self.username=username
         self.password=password
+        self.is_logged_in=False
         self.bot=webdriver.Firefox()
     
     #intializing login function
     def login(self):
+
+         
         bot=self.bot
         bot.get("https://twitter.com/login")
         time.sleep(3)
@@ -26,13 +34,56 @@ class TwitterBot:
 
         #sending username and password to those located fields 
         email.send_keys(self.username)
+        time.sleep(3)
         password.send_keys(self.password)
-        time.sleep(1)
-        password.send_keys(Keys.ENTER)
-        
-
+        password.send_keys(Keys.RETURN)
+        time.sleep(10)
+        self.is_logged_in = True
 
     
+    #making bot like and retweet tweets
+    def like_retweet(self,hashtag,load_pages):
 
+        #raising exception if not logged in
+        if not self.is_logged_in:
+            raise Exception("You must log in first!")
+
+        bot=self.bot
+
+        #opening hashtag page 
+        bot.get(f"https://twitter.com/search?q={hashtag}&src=typed_query&f=live")
+        time.sleep(4)
+
+        
+        # scrolling  pages and liking and retweeting tweets of same hashtag 
+        for i in range(load_pages):
+            try:
+
+                #liking tweet
+                bot.find_element_by_xpath("//div[@data-testid='like']").click()
+                time.sleep(3)
+
+                #retweeting tweet
+                retweet=bot.find_element_by_xpath("//div[@data-testid='retweet']")
+                action=ActionChains(bot)
+                action.click(on_element=retweet).perform()
+
+                # retweet.click()
+                # retweet.send_keys("Retweet")
+                time.sleep(2)
+
+            except:
+                time.sleep(5)
+            
+            time.sleep(3)
+
+            #scrollling pages to load more content
+            bot.execute_script('window.scrollTo(0,document.body.scrollHeight/1.75)')
+            time.sleep(5)
+
+         
+
+        
 bot=TwitterBot(username,password)
 bot.login()
+bot.like_retweet(hashtag,load_pages)
